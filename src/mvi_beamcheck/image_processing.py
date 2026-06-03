d71ef34ff9df8996ef8c5bccd6361c1499e65f18
Guido C. Hilgers <g.hilgers@radiotherapiegroep.nl>
2026-06-03 07:20:25 +0200

    refactor: remove unused image_processing file

--- src/mvi_beamcheck/image_processing.py
+++
@@ -1,5 +0,0 @@
-"""
-Contains the response conversion and singal extraction logic. This includes the
-output ROI and the five ROIS for diagonal normalized flatness, together with 
-private helper functions.
-"""
