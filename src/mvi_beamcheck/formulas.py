7c9f1aec7f85ef089e2e033c2317840c1ec1e984
Guido C. Hilgers <g.hilgers@radiotherapiegroep.nl>
2026-06-19 10:21:06 +0200

    refactor(formulas): improve output deviation implementation and add reference docstring

--- src/mvi_beamcheck/formulas.py
+++ src/mvi_beamcheck/formulas.py
@@ -1,3 +1,11 @@
 def compute_output_deviation(output_response: float, crosscal_response: float, crosscal_output: float, target_output: float) -> float:
+    """Compute output deviation in percent.
+
+    Implements Eq. 1 from Hilgers et al., 2023.
+    DOI: 10.1016/j.phro.2023.100411
+    """
     output = output_response / crosscal_response * crosscal_output
-    return (output - target_output) / target_output * 100
+    deviation = output - target_output
+    deviation_in_pct = deviation / target_output * 100
+
+    return deviation_in_pct
