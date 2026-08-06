74c146529c9685b3451489c489745594901c1298
Guido C. Hilgers <g.hilgers@radiotherapiegroep.nl>
2026-08-06 10:55:04 +0200

    feat: add BeamQualityConfig dataclass

--- src/mvi_beamcheck/config.py
+++ src/mvi_beamcheck/config.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 
+@dataclass
+class ImagerConfig:
+    mean_isocenter_pixel: tuple[float, float]
 
 @dataclass
 class OutputConfig:
@@ -7,32 +10,33 @@ class OutputConfig:
     crosscal_output: float
     target_output: float
 
-
 @dataclass
-class ImagerConfig:
-    mean_isocenter_pixel: tuple[float, float]
-
+class BeamQualityConfig:
+    reference_flatness: float
+    beta: float
 
 @dataclass
 class BeamCheckConfig:
-    output: OutputConfig
     imager: ImagerConfig
+    output: OutputConfig
+    beam_quality: BeamQualityConfig
 
     @classmethod
     def from_dict(cls, config: dict) -> 'BeamCheckConfig':
         return cls(
+            imager = ImagerConfig(
+                mean_isocenter_pixel = config['imager']['mean_isocenter_pixel']
+            ),            
             output = OutputConfig(
                 crosscal_response = config['output']['crosscal_response'],
                 crosscal_output = config['output']['crosscal_output'],
                 target_output = config['output']['target_output']
             ),
-            imager = ImagerConfig(
-                mean_isocenter_pixel = config['imager']['mean_isocenter_pixel']
+            beam_quality = BeamQualityConfig(
+                reference_flatness = config['beam_quality']['reference_flatness'],
+                beta = config['beam_quality']['beta']
             ),
         )
-    
-        
-    
                    
                                                                           
                                                                           
