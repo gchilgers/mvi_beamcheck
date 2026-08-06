f8d5a554e3c9e36ce8910a418861fc0725285ee4
Guido C. Hilgers <g.hilgers@radiotherapiegroep.nl>
2026-08-06 12:33:01 +0200

    feat: add flatness computation formula

--- src/mvi_beamcheck/formulas.py
+++ src/mvi_beamcheck/formulas.py
@@ -9,3 +9,22 @@ def compute_output_deviation(output_response: float, crosscal_response: float, c
     deviation_in_pct = deviation / target_output * 100
 
     return deviation_in_pct
+
+def compute_flatness(flatness_responses: dict) -> float:
+    """ Compute diagonal normalized flatness.
+    
+    Implements Eq. 2 from Hilgers et al., 2026.
+    DOI: 10.1016/j.phro.2026.100930
+    """
+    cax_response = flatness_responses['flat_cax']
+
+    off_axis_responses = [
+        flatness_responses[key]
+        for key in ('flat_D1', 'flat_D2', 'flat_D3', 'flat_D4')
+    ]
+
+    flatness = sum(off_axis_responses) / (len(off_axis_responses) * cax_response)
+    return flatness
+
+
+
